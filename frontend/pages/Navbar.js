5d1a35d7e70d4e0b9d3fd29911dbd723aee9c9e2
Nadir Ahmed <qx.nadir@gmail.com>
2022-10-06 00:38:42 +0600

    implement navbar

--- frontend/pages/Navbar.js
+++ frontend/pages/Navbar.js
@@ -7,88 +7,69 @@ import styles from '../styles/Navbar.module.css'
 import logo from '../assets/logo.png';
 import { Drawer } from 'antd';
 import { MdOutlineClose } from 'react-icons/md';
-import CartDrawer from './CartDrawer';
+import { FaUserCircle } from 'react-icons/fa';
+import { BiMenu } from 'react-icons/bi';
+import { HiSearch } from 'react-icons/hi';
+
+
+
+
+import Image from 'next/image'
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const [open, setOpen] = useState(false)
+    // const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
     const [cartOpen, setCartOpen] = useState(false)
     const [keyword, setKeyword] = useState('');
 
-    const menuLists = <>
+    /* const menuLists = <>
         <button className='text-white block md:hidden' onClick={()=>setOpen(!open)}><BiSearchAlt2 className='text-2xl'/></button>
-        <div className="flex relative">
-            <span onClick={()=>setCartOpen(true)} >
-                <BsCartCheckFill className='cursor-pointer text-2xl text-white' />
-            </span>
-            <span className="absolute top-[-11px] left-[13px] bg-[#669900]  rounded-full w-[20px] h-[20px] flex items-center justify-center p-[3px]">1</span>
-        </div>
-        <div>
-            <div className="dropdown">
-                <CgProfile className='dropBtn text-white' />
-                <div className="dropdown-content">
-                    <ul>
-                        <li onClick={()=>navigate('/profile')}>Profile</li>
-                        <li>Profile</li>
-                        <li onClick={()=>navigate('/login')}>Login</li>
-                    </ul>
-                    
-                        {/* {
-                            user && <Link to='/dashboard' className='cursor-pointer hover:bg-gray-200 p-2'>
-                                <label htmlFor="dashboardDrawer" className=" drawer-button">Dashboard</label>
-                            </Link>
-                        }
-                        {
-                            user ?
-                            <button onClick={handleSignOut} className='cursor-pointer hover:bg-gray-200 p-2'>Sign out</button>
-                            : 
-                            <Link to='/login' className='cursor-pointer hover:bg-gray-200 p-2'>Login</Link>
-                        } */}
-                </div>
-            </div>
-        </div> 
-    </>
+        
+    </> */
     return (
-        <div className='bg-[#081621] '>
+        <div className='border'>
             <div className='max-w-7xl mx-auto px-2 '>
-                <div className=' flex items-center h-14 justify-between text-black  relative z-50'>
-                    <div className=''>
-                        <img className='cursor-pointer w-[95px]' onClick={()=>navigate('/home')} src={logo} alt="" />
+                <div className='flex items-center justify-between text-black '>
+                    <div className='flex justify-start'>
+                        <Image
+                            src={logo}
+                            alt="Picture of the author"
+                        />
                     </div>
-                    <div className='searchContainer'>
-                        <div className='hidden md:block'>
-                            <div className='flex items-center '>
-                                <input className='searchInput' onChange={(e)=>setKeyword(e.target.value)} type="text" placeholder='KHUJ, THE SEARCH' />
-                                <button className='searchBtn' onClick={()=>navigate(`/search/${keyword}`)}><BiSearchAlt2/></button>
+                    <div className={styles.searchContainer}>
+                        <div className='flex items-center gap-4'>
+                            <div>
+                                Anywhere
+                            </div>
+                            <div className={styles.hr}>
+                                <p></p>
+                            </div>
+                            <div>
+                                Any week
+                            </div>
+                            <div className={styles.hr}>
+                                <p></p>
+                            </div>
+                            <div>
+                                Add Guests
+                            </div>
+                            <div className={styles.searchBtn}>
+                                <HiSearch className={styles.icon} />
                             </div>
                         </div>
                     </div>
                     <div className='flex gap-5 items-center'>
-                        {menuLists}
-                    </div>
-                </div>
-                <div className=' searchBlock'>
-                    {
-                        open && <div className='bg-white'>
-                        <div className='flex items-center '>
-                            <input className='searchInput' onChange={(e)=>setKeyword(e.target.value)} type="text" placeholder='KHUJ, THE SEARCH' />
-                            <button className='searchBtn' onClick={()=>navigate(`/search/${keyword}`)}><BiSearchAlt2/></button>
+                         <div className={styles.userMenu} onClick={()=>setDropdown(!dropdown)}>
+                            <BiMenu className='cursor-pointer text-xl '/>
+                            <span onClick={()=>setCartOpen(true)} >
+                                <FaUserCircle className=' text-2xl ' />
+                            </span>
                         </div>
+                        
                     </div>
-                    }
                 </div>
             </div>
-            <Drawer
-                className='drawer'
-                bodyStyle={{"padding": "0px"}}
-                headerStyle={{"borderBottom": "0px ", "display": "none"}}
-                placement="right" closeIcon={false} visible={cartOpen}>
-                    <div  className='drawerHeader'>
-                        <h1 className=' text-white'>YOUR CART</h1>
-                        <MdOutlineClose className='cartCloseBtn' onClick={()=>setCartOpen(false)} />
-                    </div>
-                    <CartDrawer/>
-            </Drawer>
         </div>
     );
 };
