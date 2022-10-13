e34d3a48e623fb817a716fedf0078722e6d53b81
Nadir Ahmed <qx.nadir@gmail.com>
2022-10-14 02:39:22 +0600

    slider implement

--- frontend/pages/home.jsx
+++ frontend/pages/home.jsx
@@ -22,38 +22,43 @@ export default function card() {
 
     const settings = {
         arrows: true,
+        dots: true,
         prevArrow: <ArrowLeft />,
         nextArrow: <ArrowRight />,
+        appendDots: dots => (
+            <div
+              style={{position:"absolute", bottom:"8px", }}>
+              <ul className={style.sideDots} style={{ margin: "0px" }}> <span className={style.dots}>{dots}</span> </ul>
+            </div>
+          )
     }; 
   return (
     <div className="max-w-7xl mx-auto px-10 mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
             {
                 palace.map((item)=> 
-                    <div key={item.id}>
-                        <div className={style.cardItem}>
-                            <div>
-                                <Slider {...settings}>
-                                    {
-                                        item.img.map((another)=>
-                                        <img className="h-[250px] rounded-xl" src={another} alt="" />
-                                        )
-                                    }
-                                </Slider>
-                            </div>
-                            <div className="flex mt-3 items-center justify-between  font-bold">
-                                <p className="m-0 text-[15px]">{item.name}</p>
-                                <div className="flex items-center gap-1">
-                                    <FaStar className="text-[13px]"/>
-                                    <span className="text-[14px] font-semibold">4.96</span>
-                                </div>
-                            </div>
-                            <p className="m-0 text-[15px] text-[#717175]">3,390 kilometers away</p>
-                            <p className="m-0 text-[15px] text-[#717175]">Nov 7-12</p>
+                    <div key={item.id} className={style.cardItem}>
+                        <div>
+                            <Slider {...settings}>
+                                {
+                                    item.img.map((another)=>
+                                    <img className="h-[250px] rounded-xl" src={another} alt="" />
+                                    )
+                                }
+                            </Slider>
+                        </div>
+                        <div className="flex mt-3 items-center justify-between  font-bold">
+                            <p className="m-0 text-[15px]">{item.name}</p>
                             <div className="flex items-center gap-1">
-                                <span className="text-[15px] font-semibold">$1,116 AUD</span>
-                                <span>night</span>
+                                <FaStar className="text-[13px]"/>
+                                <span className="text-[14px] font-semibold">4.96</span>
                             </div>
+                        </div>
+                        <p className="m-0 text-[15px] text-[#717175]">3,390 kilometers away</p>
+                        <p className="m-0 text-[15px] text-[#717175]">Nov 7-12</p>
+                        <div className="flex items-center gap-1">
+                            <span className="text-[15px] font-semibold">$1,116 AUD</span>
+                            <span>night</span>
                         </div>
                     </div>
                 )
